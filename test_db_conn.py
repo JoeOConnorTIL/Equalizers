8a7c58f7bb9b26955391dace6e00d71111b53759
JoeOConnorTIL <jpoc1g11@gmail.com>
2026-08-25 10:47:28 +0100

    Created new database, successfully tested connection and a test upload

---
+++ test_db_conn.py
@@ -0,0 +1,18 @@
+import duckdb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+motherduck_token= os.getenv('MOTHERDUCK_DB_TOKEN')
+
+con = duckdb.connect(f'md:?motherduck_token={motherduck_token}')
+
+con.sql("SHOW DATABASES").show()
+con.sql("USE my_db")
+con.sql("SELECT current_database()").show()
+
+con.execute("""
+    CREATE TABLE IF NOT EXISTS my_db.test_upload AS
+    SELECT * FROM './sample_data/test.json'
+"""
+)
