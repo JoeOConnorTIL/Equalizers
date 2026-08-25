5f64cc80e43e29c7a11cbc264f13c2d42b487648
JoeOConnorTIL <jpoc1g11@gmail.com>
2026-08-25 13:51:08 +0100

    generated dbt project within repo

--- test_db_conn.py
+++ test_db_conn.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-motherduck_token= os.getenv('MOTHERDUCK_DB_TOKEN')
+motherduck_token= os.getenv('MOTHERDUCK_TOKEN')
 
 con = duckdb.connect(f'md:?motherduck_token={motherduck_token}')
 
