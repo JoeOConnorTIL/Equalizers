5b181488ecef2f70ec90c23d2f843bfada556717
JoeOConnorTIL <jpoc1g11@gmail.com>
2026-08-25 15:46:06 +0100

    testing db connection with dbt - reconfigured env variable

--- dbt_project/analyses/analyses.sql
+++ dbt_project/analyses/analyses.sql
@@ -1,1 +1,2 @@
-{{ codegen.generate_source(schema_name= 'main', database_name= 'my_db', table_names=['test_upload']) }}
+{#  {{ codegen.generate_source(schema_name= 'main', database_name= 'my_db', table_names=['test_upload']) }} #}
+
